1118755d9f843034d3eecbae2434996b38e0cca8
yomaswn <yaumashf@gmail.com>
2017-02-15 06:04:31 +0700

    copy from twatt-new

---
+++ server/routes/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+var config = require('../config.json')
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Welcome yomatwit' });
+});
+
+var Twit = require('twit')
+
+var client = new Twit({
+  consumer_key:         config.consumer_key,
+  consumer_secret:      config.consumer_secret,
+  access_token:         config.access_token,
+  access_token_secret:  config.access_token_secret,
+})
+
+router.get('/tweet', function(req,res,next){
+  let quer = {status: req.query.q}
+  client.post('statuses/update', quer, function(error, data, response) {
+    if (error) throw error;
+  res.send(data)
+  })
+})
+
+module.exports = router;
